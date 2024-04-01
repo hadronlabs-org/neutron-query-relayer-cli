53cc793a7f72e0c12ff0641895a268246ee4f874
Albert Andrejev <albert.andrejev@gmail.com>
2024-04-01 21:51:19 +0300

    add sleep

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 NODE=${NODE:-node}
 echo "Waiting for a first block..."
 while ! curl -f ${NODE}:1317/cosmos/base/tendermint/v1beta1/blocks/1 >/dev/null 2>&1; do
   sleep 1
 done
 echo "Start relayer coordinator"
-cd /coordinator
-yarn dev
+while true; do
+  echo "Working..."
+  sleep 5
+done
+
+#cd /coordinator
+#yarn dev
